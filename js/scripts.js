c54e7035a26d7bcf956397bae42cad0a7092e672
Ayana Powell and Sara Jensen <ayana+sara@>
2016-08-16 16:23:44 -0700

    word puzzle

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,14 @@ $(document).ready(function() {
       number1,
       number2;
 
+  $('.math').click(function() {
+    $('form#input1').fadeIn(1000);
+  });
+  $('.word').click(function() {
+    $('form#input2').fadeIn(1000);
+  });
+
+  
   function math() {
     if (number1 < number2) {
       alert("This doesn't work");
@@ -26,11 +34,31 @@ $(document).ready(function() {
   });
 
   $('form#input2').submit(function(event) {
+    var newSentence;
     event.preventDefault();
-    var sentence = $("#sentence").val().replace('e,' , '-'),
-        newSentence = sentence.split();
-    console.log(newSentence);
+    var letters = $("#sentence").val().split("");
+    for (i = 0; i <= letters.length; i++) {
+      vowels();
+    }
 
+    function vowels() {
+      if (letters[i] === "a" || letters[i] === "e" || letters[i] === "i" || letters[i] === "o" || letters[i] === "u") {
+        letters.splice(i, 1, "-");
+        newSentence = letters.join("");
+      } else {
+        return false;
+      };
+    };
+      $("#sentenceOutput").append("<p>" + newSentence + "</p>");
   });
 
 });
+
+
+/*  $('form#input2').submit(function(event) {
+    event.preventDefault();
+    var sentence = $("#sentence").val();
+    for (i = 0; i < sentence.length; i++) {
+      alert(sentence[4]);
+    };
+  });*/
